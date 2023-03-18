0e4367b0e2113b1748d650a1d3d830a89ad14172
sahirbhat <sahirbhat007@gmail.com>
2023-03-18 16:29:20 +0530

    ip address downgraded

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-98&#d8^8+3z@au-$1ju$q@5kv13#fapquc%kpsrb*5j&h+v-ot
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
